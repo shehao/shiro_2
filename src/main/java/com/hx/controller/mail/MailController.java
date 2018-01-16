e7debb286ad44c6ee00b84a53b115366c2780260
shehao <13750020744@163.com>
2018-01-16 10:47:59 +0800

    新增加发送邮件

--- src/main/java/com/hx/controller/mail/MailController.java
+++ src/main/java/com/hx/controller/mail/MailController.java
@@ -1,10 +1,32 @@
 package com.hx.controller.mail;
 
+import java.util.LinkedList;
+
 /**
  * @Author: shehao
  * @Description:
  * @Date: Created in 2018/1/15  10:01
  */
 public class MailController {
+    public static void main(String[] args) {
+       /* LinkedList list=new LinkedList();
+        list.add("a");
+       // list.add(2,"b");
+      //  String s=(String)list.get(1);
+       // System.out.println(s);
+        System.out.println(""+'a'+1);*/
+        System.out.println(cout());
+    }
+
+    public  static  int cout(){
+        try {
+            return 5/0;
+
+        }catch (Exception e){
+            return  3*2;
+        }finally {
+            return 3;
+        }
+    }
 
 }
